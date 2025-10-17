579024b14b14c21f61bb01de7a9d3d190004fa27
Hugo Miquilino <hugomiquilino@gmail.com>
2025-10-17 17:42:51 -0300

    Initializes project structure
    Sets up the basic project structure.
    
    Adds a .gitignore file to exclude unnecessary files and
    directories from version control.
    
    Creates a main script that imports modules and includes a main loop.

---
+++ src/main.py
@@ -0,0 +1,13 @@
+from modules import *
+
+def mainloop():
+
+    sti_data = sti_module()
+    plan_data = plan_module()
+    
+
+    print(sti_data).all()
+
+
+if __name__ == "__main__":
+    mainloop()
