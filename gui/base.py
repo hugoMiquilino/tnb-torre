f0da2d950e5b1e4612e94724347fb26bcd8bf4d4
Hugo Miquilino <hugomiquilino@gmail.com>
2025-12-19 10:30:30 -0300

    Add application icon and enhance StatusGUI with ttkbootstrap. Add some buttons for restart and close functionality.

--- gui/base.py
+++ gui/base.py
@@ -1,26 +1,70 @@
+import sys
+import os
 import tkinter as tk
-from tkinter import ttk
+from ttkbootstrap.constants import *
+import ttkbootstrap as ttk
+from pathlib import Path
 
-class StatusGUI(tk.Tk):
+class StatusGUI(ttk.Window):
     def __init__(self, queue):
-        super().__init__()
+        super().__init__(
+            title="TNB Torre - Status",
+            themename="darkly",
+            size=(700, 500),
+            resizable=(False, False),
+        )
+
         self.queue = queue
 
-        self.title("TNB Torre - Status")
-        self.geometry("600x350")
-        self.resizable("False", "False")
+        # Icon
+        icon_path = Path(__file__).parent / "assets" / "app.ico"
 
+        if icon_path.exists():
+            self.iconbitmap(icon_path)
+
+        # Status
         self.status = ttk.Label(
             self,
             text="Aguardando dados...",
-            font=("Segoe UI", 11, "bold")
+            font=("Segoe UI", 11, "bold"),
+            bootstyle=INFO,
         )
         self.status.pack(pady=10)
 
-        self.log = tk.Text(self, height=15, state="disabled")
-        self.log.pack(fill="both", padx=10)
+        # Log
+        self.log = tk.Text(
+            self,
+            height=15,
+            state="disabled",
+            bg="#1e1e1e",
+            fg="#dcdcdc",
+            insertbackground="white",
+            font=("Consolas", 10),
+            relief="flat",
+        )
+        self.log.pack(fill=BOTH, expand=True, padx=10, pady=(0, 10))
 
         self.after(200, self.poll_queue)
+
+        # Botões inferiores
+        btn_frame = ttk.Frame(self)
+        btn_frame.pack(fill=X, padx=10, pady=10)
+
+        ttk.Button(
+            btn_frame, text="🔄 Reiniciar", bootstyle=(INFO, OUTLINE), command=self.restart_app
+        ).pack(side=LEFT)
+
+        ttk.Button(
+            btn_frame, text="❌ Encerrar", bootstyle=(DANGER, OUTLINE), command=self.close_app
+        ).pack(side=RIGHT)
+
+    def restart_app(self):
+        python = sys.executable
+        os.execl(python, python, *sys.argv)
+
+    def close_app(self):
+        self.destroy()
+        os._exit(0)
 
     def poll_queue(self):
         while not self.queue.empty():
